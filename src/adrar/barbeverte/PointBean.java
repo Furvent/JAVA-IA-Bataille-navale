7f2cd34954847b0f17674251075a1e8d17cab8f3
Salim <salim-mohamed@MacBook-Pro-de-Salim.local>
2019-03-19 16:02:58 +0100

    Ajout de la class PointBean

---
+++ src/adrar/barbeverte/PointBean.java
@@ -0,0 +1,34 @@
+package adrar.barbeverte;
+
+public class PointBean {
+
+	private int axeX;
+	private int axeY;
+
+	// ---------------------
+	// ---GETTER SETTER-----
+	// ---------------------
+
+	public PointBean(int axeX, int axeY) {
+		super();
+		this.axeX = axeX;
+		this.axeY = axeY;
+	}
+
+	public int getAxeX() {
+		return axeX;
+	}
+
+	public void setAxeX(int axeX) {
+		this.axeX = axeX;
+	}
+
+	public int getAxeY() {
+		return axeY;
+	}
+
+	public void setAxeY(int axeY) {
+		this.axeY = axeY;
+	}
+
+}
